df92e820a2dd773831022f4229c233f34c207cb2
Jeong-seyuong <jsujsu4164@naver.com>
2020-12-02 03:14:40 +0900

    html,css 3차 구현

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 const $accordionList = document.querySelector('.accordion-list')
 
 $accordionList.onclick = (e) => {
+  
   if(!e.target.matches('.accordion-list>li')) return;
+  if(e.target.matches('.accordion-list>li.active')) return;
   // [...$accordionList.children].forEach($li => $li.classList.remove('active'))
-  e.target.classList.add('active')
-  console.log(e.target.previoussibling)
+  [...$accordionList.children].forEach(($li,index)=> {
+    if(e.target === $li) {
+      for(let i = 0 ;i<=index;i++){
+        $accordionList.children[i].classList.add('active')
+        $accordionList.children[i].style.left = `${(5*i)}%`
+        $accordionList.children[index].animate([
+          { transform: 'translateX(100%)' }, 
+          { transform: 'translateX(0)'}
+        ],{
+          duration:500,
+        })
+      }
+      
+    };
+  })
+
+ 
 }
